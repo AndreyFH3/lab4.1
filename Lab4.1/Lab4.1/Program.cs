d5cf64dbdb7dd606d5e87ed9305e29f35163e988
AndreyFH3 <andrey.likhnitskiy@gmail.com>
2020-11-16 21:44:06 +0700

    firstStart

---
+++ Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+}
